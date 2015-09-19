89e03c4d69938a05d21d9817578b275f3cc2410f
Sarah <tzawah@Sarahs-Mac-mini.local>
2015-09-18 19:36:33 -0700

    Mostly done here setting up the parse client.  I haven't tested this yet but I can start hooking it up to the UI at this point and then test getting and posting locations.

--- OnTheMap/ParseConvenience.swift
+++ OnTheMap/ParseConvenience.swift
@@ -6,4 +6,92 @@
 //  Copyright (c) 2015 SarahHowe. All rights reserved.
 //
 
+import UIKit
 import Foundation
+
+extension ParseClient {
+    
+    //MARK: Student Location Methods
+    
+    func getStudentLocation(completionHandler: (result: [StudentLocation]?, error: NSError?) -> Void)
+    {
+        //specify parameters and method
+        let parameters = [
+            ParseClient.ParameterKeys.Limit: "\(100)",
+            ParseClient.ParameterKeys.Skip: "\(0)",
+            ParseClient.ParameterKeys.Order: "-updatedAt"
+        ]
+        
+        let method : String = Methods.StudentLocation
+        
+        //make the request
+        taskForGetMethod(method, parameters: parameters) { JSONResult, error in
+            
+            //send the desired values to the completion handler
+            if let error = error
+            {
+                completionHandler(result: nil, error: error)
+            }
+            else
+            {
+                if let results = JSONResult.valueForKey(ParseClient.JSONResponseKeys.Results) as? [[String : AnyObject]]
+                {
+                    var locations = StudentLocation.locationsFromResults(results)
+                    
+                    completionHandler(result: locations, error: nil)
+                }
+                else
+                {
+                    completionHandler(result: nil, error: NSError(domain: "getStudentLocation parsing", code: 0, userInfo: [NSLocalizedDescriptionKey : "Could not parse getStudentLocation"]))
+                }
+            }
+        }
+    }
+    
+    func postStudentLocation(location: StudentLocation, completionHandler: (result: String?, error: NSError?) -> Void)
+    {
+        //specify method and HTTP body
+        let method : String = Methods.StudentLocation
+        
+        let jsonBody: [String : AnyObject] = [
+            ParseClient.JSONBodyKeys.UniqueKey: location.uniqueKey,
+            ParseClient.JSONBodyKeys.FirstName: location.firstName,
+            ParseClient.JSONBodyKeys.LastName: location.lastName,
+            ParseClient.JSONBodyKeys.MapString: location.mapString,
+            ParseClient.JSONBodyKeys.MediaURL: location.mediaURL,
+            ParseClient.JSONBodyKeys.Latitude: location.latitude! as Double,
+            ParseClient.JSONBodyKeys.Longitude: location.longitude! as Double
+        ]
+        
+        //make the request
+        taskForPostMethod(method, jsonBody: jsonBody) { JSONResult, error in
+            
+            //send the desired values to the completion handler
+            if let error = error
+            {
+                completionHandler(result: nil, error: error)
+            }
+            else
+            {
+                if let objectID = JSONResult.valueForKey(ParseClient.JSONResponseKeys.ObjectID) as? String
+                {
+                    if let createdAt = JSONResult.valueForKey(ParseClient.JSONResponseKeys.CreatedAt) as? String
+                    {
+                        println("Student Location Posted \(objectID) \(createdAt)")
+                        
+                        completionHandler(result: objectID, error: nil)
+                    }
+                    else
+                    {
+                        completionHandler(result: nil, error: NSError(domain: "postStudentLocation parsing", code: 0, userInfo: [NSLocalizedDescriptionKey: "couldn't find createdAt key in result"]))
+                    }
+                }
+                else
+                {
+                    completionHandler(result: nil, error: NSError(domain: "postStudentLocation parsing", code: 0, userInfo: [NSLocalizedDescriptionKey: "couldn't find objectID key in result"]))
+                }
+            }
+        }
+    }
+    
+}
---
+++ OnTheMap/StudentLocation.swift
@@ -0,0 +1,49 @@
+//
+//  StudentLocation.swift
+//  OnTheMap
+//
+//  Created by Sarah on 9/18/15.
+//  Copyright (c) 2015 SarahHowe. All rights reserved.
+//
+
+struct StudentLocation {
+    
+    var firstName = ""
+    var lastName = ""
+    var latitude: Double? = nil
+    var longitude: Double? = nil
+    var mapString = ""
+    var mediaURL = ""
+    var objectID = ""
+    var uniqueKey = ""
+    var createdAt = ""
+    var updatedAt = ""
+    
+    //construct a Student Location result from a dictionary
+    init(dictionary: [String : AnyObject])
+    {
+        firstName = dictionary[ParseClient.JSONResponseKeys.FirstName] as! String
+        lastName = dictionary[ParseClient.JSONResponseKeys.LastName] as! String
+        latitude = dictionary[ParseClient.JSONResponseKeys.Latitude] as? Double
+        longitude = dictionary[ParseClient.JSONResponseKeys.Longitude] as? Double
+        mapString = dictionary[ParseClient.JSONResponseKeys.MapString] as! String
+        mediaURL = dictionary[ParseClient.JSONResponseKeys.MediaURL] as! String
+        objectID = dictionary[ParseClient.JSONResponseKeys.ObjectID] as! String
+        uniqueKey = dictionary[ParseClient.JSONResponseKeys.UniqueKey] as! String
+        createdAt = dictionary[ParseClient.JSONResponseKeys.CreatedAt] as! String
+        updatedAt = dictionary[ParseClient.JSONResponseKeys.UpdatedAt] as! String
+    }
+    
+    //given an array of dictionaries, convert them to an array of Student Location result objects
+    static func locationsFromResults(results: [[String : AnyObject]]) -> [StudentLocation]
+    {
+        var studentLocations = [StudentLocation]()
+        
+        for result in results
+        {
+            studentLocations.append(StudentLocation(dictionary: result))
+        }
+        
+        return studentLocations
+    }
+}
